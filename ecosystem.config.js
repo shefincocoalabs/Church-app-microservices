249f15847a271e41ee2560ecb0cf97906b85b1cc
Shefin S <shefin@cocoalabs.in>
2020-08-04 15:12:53 +0530

    ecosystem changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,6 +27,76 @@ module.exports = {
         NODE_ENV: 'development',
         port : 3002
       }
+    },
+    {
+      name: 'Events church-app Microservices',
+      script: 'events.service.js',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      //cron_restart
+      env: {
+        NODE_ENV: 'development',
+        port : 3003
+      }
+    },
+    {
+      name: 'Groups church-app Microservices',
+      script: 'groups.service.js',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      //cron_restart
+      env: {
+        NODE_ENV: 'development',
+        port : 3004
+      }
+    },
+    {
+      name: 'Masters church-app Microservices',
+      script: 'masters.service.js',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      //cron_restart
+      env: {
+        NODE_ENV: 'development',
+        port : 3005
+      }
+    },
+    {
+      name: 'Buy/sell church-app Microservices',
+      script: 'buyorsell.service.js',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      //cron_restart
+      env: {
+        NODE_ENV: 'development',
+        port : 3006
+      }
+    },
+    {
+      name: 'Feeds church-app Microservices',
+      script: 'feeds.service.js',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      //cron_restart
+      env: {
+        NODE_ENV: 'development',
+        port : 3007
+      }
     }
     ]
   };
