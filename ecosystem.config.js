cb92224afd318636c4b2ed21e75b91594aa4b959
Shefin S <shefin@cocoalabs.in>
2020-08-04 21:07:39 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -125,6 +125,20 @@ module.exports = {
         NODE_ENV: 'development',
         port : 3009
       }
+    },
+    {
+      name: 'Charity church-app Microservices',
+      script: 'charities.service.js',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      //cron_restart
+      env: {
+        NODE_ENV: 'development',
+        port : 3010
+      }
     }
     ]
   };
