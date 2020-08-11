c09f714fba8930ebea5396e9dfc9c4d3abc0cbf3
Shefin S <shefin@cocoalabs.in>
2020-08-11 13:36:48 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -139,6 +139,20 @@ module.exports = {
         NODE_ENV: 'development',
         port : 3010
       }
+    },
+    {
+      name: 'Urogulf church-app Microservices',
+      script: 'urogulf.service.js',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      //cron_restart
+      env: {
+        NODE_ENV: 'development',
+        port : 3011
+      }
     }
     ]
   };
