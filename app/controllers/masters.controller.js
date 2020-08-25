b344fc631238da584d319080b33515db8620cda1
Rahul <rahul.cocoalabs.com>
2020-08-25 10:18:23 +0530

    change

--- app/controllers/masters.controller.js
+++ app/controllers/masters.controller.js
@@ -1,4 +1,5 @@
 var Church = require('../models/church.model');
+var User = require('../models/user.model');
 var Parish = require('../models/parish.model');
 var ParishWard = require('../models/parishWard.model');
 
@@ -10,7 +11,7 @@ exports.churchList = async (req, res) => {
         var projection = {
             name: 1
         };
-        var listChurch = await Church.find(filter,projection).sort({
+        var listChurch = await Church.find(filter, projection).sort({
             'tsCreatedAt': -1
         });
         res.status(200).send({
@@ -71,6 +72,68 @@ exports.parishWardList = async (req, res) => {
         res.status(500).send({
             success: 0,
             message: err.message
+        })
+    }
+}
+
+exports.getKey = async (req, res) => {
+    var identity = req.identity.data;
+    var userId = identity.id;
+
+    let userData = await User.findOne({
+        _id: userId,
+        status: 1
+    })
+        .catch(err => {
+            return {
+                success: 0,
+                message: 'Something went wrong while getting user data',
+                error: err
+            }
+        })
+    if (userData && (userData.success !== undefined) && (userData.success === 0)) {
+        return res.send(userData);
+    }
+    if (userData) {
+        let churchId = userData.church;
+        let churchData = await Church.findOne({
+            _id: churchId,
+            status: 1
+        })
+            .catch(err => {
+                return {
+                    success: 0,
+                    message: 'Something went wrong while getting user data',
+                    error: err
+                }
+            })
+        if (churchData && (churchData.success !== undefined) && (churchData.success === 0)) {
+            return res.send(churchData);
+        }
+        if (churchData) {
+            let paymentGatewayKey = churchData.paymentGatewayKey;
+            if(paymentGatewayKey){
+            return res.send({
+                success : 1,
+                paymentGatewayKey,
+                message : 'Payment gateway key '
+            })
+        }else{
+            return res.send({
+                success: 0,
+                message: 'Online payement not enabled'
+            })
+        }
+        } else {
+            return res.send({
+                success: 0,
+                message: 'Church not found'
+            })
+        }
+    } else {
+        return res.send({
+            success: 0,
+            message: 'User not found'
         })
     }
 }
--- app/routes/masters.routes.js
+++ app/routes/masters.routes.js
@@ -1,6 +1,9 @@
+const auth = require('../middleware/auth.js');
+
 module.exports = (app) => {
     const masters = require('../controllers/masters.controller');
     app.get('/masters/church/list', masters.churchList);
     app.get('/masters/:id/parish/list', masters.parishList);
     app.get('/masters/:id/parish-ward/list', masters.parishWardList);
+    app.get('/masters/payment-gatway/key',auth, masters.getKey);
 };
