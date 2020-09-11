87bce1e95204ba1310aad245c35d0e0ad8a13aa3
Rahul <rahul.cocoalabs.com>
2020-09-11 16:58:33 +0530

    change

---
+++ app/models/groupMessage.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+function transform(ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.status;
+    delete ret.tsCreatedAt;
+    delete ret.tsModifiedAt;
+}
+var options = {
+    toObject: {
+        virtuals: true,
+        transform: function (doc, ret) {
+            transform(ret);
+        }
+    },
+    toJSON: {
+        virtuals: true,
+        transform: function (doc, ret) {
+            transform(ret);
+        }
+    }
+};
+
+const GroupMessageSchema = mongoose.Schema({
+    groupId:  {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Group'
+    },
+    userId:  {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    userName : String,
+    socketId : String,
+    content: String,
+    date: Date,
+    status: Number,
+    tsCreatedAt: Number,
+    tsModifiedAt: Number
+
+}, options);
+module.exports = mongoose.model('GroupMessage', GroupMessageSchema, 'GroupMessages');
